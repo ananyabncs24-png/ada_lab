5a70876c7d7ef265369aa49c7d2cb7aed719d501
ananyabn0804 <ananyabn.cs24@bmsce.ac.in>
2026-06-07 11:22:25 +0530

    Update Johnson_trotter.c

--- Johnson_Trotter/Johnson_trotter.c
+++ Johnson_Trotter/Johnson_trotter.c
@@ -1,34 +1,101 @@
-#include <stdio.h>
-#include <stdlib.h>
-void swap(int* a, int* b) {
-int temp = *a;
-*a = *b;
-*b = temp;
+#include<stdio.h>
+
+#define LEFT 0
+#define RIGHT 1
+#define MAX 100
+
+int val[MAX];
+int dir[MAX];
+
+void printpermutation(int n)
+{
+    for(int i=1;i<=n;i++)
+    {
+        printf("%d ",val[i]);
+    }
+    printf("\n");
+
 }
-void generatePermutations(int arr[], int start, int end) {
-if (start == end) {
-for (int i = 0; i <= end; i++) {
-printf("%d ", arr[i]);
+void johnson_trotter(int n)
+{
+    
+    for(int i=1;i<=n;i++)
+    {
+        val[i]=i;
+        dir[i]=LEFT;
+    }
+    printpermutation(n);
+    while(1)
+    {
+        int mobile_val=-1;
+        int mobile_idx=-1;
+        
+        for(int i=1;i<=n;i++)
+        {
+            if(dir[i]==LEFT && i>1)
+            {
+                if(val[i]>val[i-1] && val[i]>mobile_val)
+                {
+                    mobile_val=val[i];
+                    mobile_idx=i;
+                }
+            }
+            else if(dir[i]==RIGHT && i<n)
+            {
+                if(val[i]>val[i+1] && val[i]>mobile_val)
+                {
+                    mobile_val=val[i];
+                    mobile_idx=i;
+                }
+            }
+        }
+        printf("mobile ele=%d and mobile idx=%d\n",mobile_val,mobile_idx);
+        
+        if(mobile_idx==-1)
+        {
+            break;
+        }
+
+        
+        int next_idx=(dir[mobile_idx]==LEFT)?(mobile_idx-1):(mobile_idx+1);
+
+        
+        int temp1=val[mobile_idx];
+        val[mobile_idx]=val[next_idx];
+        val[next_idx]=temp1;
+
+        int temp2=dir[mobile_idx];
+        dir[mobile_idx]=dir[next_idx];
+        dir[next_idx]=temp2;
+
+        
+        for(int i=1;i<=n;i++)
+        {
+            if(val[i]>mobile_val)
+            {
+                if(dir[i]==LEFT)
+                {
+                    dir[i]=RIGHT;
+                }
+                else
+                {
+                    dir[i]=LEFT;
+                }
+            }
+        }
+
+       
+        printpermutation(n);
+
+       
+    }
 }
-printf("\n");
-} else {
-for (int i = start; i <= end; i++) {
-swap(&arr[start], &arr[i]);
-generatePermutations(arr, start + 1, end);
-swap(&arr[start], &arr[i]); // backtrack
-}
-}
-}
-int main() {
-int n;
-printf("Enter the number of elements: ");
-scanf("%d", &n);
-int* arr = (int*)malloc(n * sizeof(int));
-printf("Enter the elements: ");
-for (int i = 0; i < n; i++) {
-scanf("%d", &arr[i]);
-}
-generatePermutations(arr, 0, n - 1);
-free(arr);
-return 0;
+int main()
+{
+    int n;
+    printf("enter the number of elements");
+    scanf("%d",&n);
+    johnson_trotter(n);
+
+    return 0;
 }
